00bf2feb5cfa114499b45224e1698a62ff21afc4
Harsh Sharma <harshsharma@jklu.edu.in>
2023-03-13 14:23:15 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Appbar from './Appbar';
+
+import Demo from './Demo';
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar/>
+      <Demo/>      
+    </div>
+  );
+}
+
+export default App;
